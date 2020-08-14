aeb860a8c46e87a91fc328668d12148b833ccc36
cycawy <cycawy@gmail.com>
2020-08-13 17:07:54 -0700

    added show/hide loading spinner, and fixed the button active box shadow issue.

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ const quoteText = document.getElementById('quote');
 const quoteAuthor = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader= document.getElementById("loader");
+
+
+//show loading
+function showLoadingSpinner(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide loading
+function removeLoadingSpinner(){
+    if(!loader.hidden){
+        loader.hidden = true;
+        quoteContainer.hidden = false;
+    }
+}
 
 //Get quote from API
 async function getQuote(){
+    showLoadingSpinner();
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
@@ -29,6 +46,7 @@ async function getQuote(){
         }
 
         quoteText.innerText = data.quoteText;
+        removeLoadingSpinner();
     }catch(error){
         getQuote();
         console.log("error!", data);
@@ -39,8 +57,8 @@ async function getQuote(){
 function tweetQuote(){
     const quote=quoteText.innerText;
     const author=quoteAuthor.innerText;
-    const tweetUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-    window.open(tweetUrl, '_blank');
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
 }
 
 //Event Listeners
